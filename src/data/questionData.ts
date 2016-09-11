89d20c66e187b57b324c41b8f4fc696ac315a577
Mike Hogarth <mikehogarth20@hotmail.com>
2016-09-11 12:03:54 +0100

    add question data

---
+++ src/data/questionData.ts
@@ -0,0 +1,58 @@
+export const QUESTIONS = {
+  1: {
+    text: "Did the murderer go to the East Side?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  2: {
+    text: "Did a male do it?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  3: {
+    text: "What area did the murderer go to?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  4: {
+    text: "Was the murder weapon a .38?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  5: {
+    text: "Where was the .38 hidden?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  6: {
+    text: "Where was the .45 hidden?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  7: {
+    text: "Which place contained only three suspects?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  8: {
+    text: "Did the murderer go to place A, B or C?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  9: {
+    text: "Were you on the east side?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  10: {
+    text: "Which area were you in?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  11: {
+    text: "Were you at place A, B or C?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  12: {
+    text: "Were you where a weapon was hidden?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  13: {
+    text: "Are an odd-numbered suspect's prints on the .38?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  },
+  14: {
+    text: "Are an odd-numbered suspect's prints on the .45?",
+    answer: (crime:any, suspect:any) : string => { return '' }
+  }
+}
