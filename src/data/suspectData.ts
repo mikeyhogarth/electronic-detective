50a2c653ac5f25d98629ad3a21a1f4745fac89d7
Mike Hogarth <mikehogarth20@hotmail.com>
2016-09-11 13:58:18 +0100

    Add suspect data

---
+++ src/data/suspectData.ts
@@ -0,0 +1,124 @@
+export enum Sex { male, female }
+
+export const SUSPECTS = {
+  1: {
+    name: "Lenny Little",
+    job: "Bartender",
+    questions: [1,2,9,13,14],
+    sex: Sex.male
+  },
+  2: {
+    name: "Al Farook",
+    job: "Tycoon",
+    questions: [2,3,9,13,14],
+    sex: Sex.male
+  },
+  3: {
+    name: "Pepe Perez",
+    job: "Flamenco Dancer",
+    questions: [3,4,5,13,14],
+    sex: Sex.male
+  },
+  4: {
+    name: "Tony Racheti",
+    job: "Promoter",
+    questions: [3,5,9,13,14],
+    sex: Sex.male
+  },
+  5: {
+    name: "Mickey O'Malley",
+    job: "Retired Cop",
+    questions: [1,4,9,13,14],
+    sex: Sex.male
+  },
+  6: {
+    name: "Max FineFlugle",
+    job: "Producer",
+    questions: [5,6,12,13,14],
+    sex: Sex.male
+  },
+  7: {
+    name: "Ripp Rapp",
+    job: "Dockworker",
+    questions: [2,5,12,13,14],
+    sex: Sex.male
+  },
+  8: {
+    name: "Buster Bailey",
+    job: "Basketball Player",
+    questions: [3,5,12,13],
+    sex: Sex.male
+  },
+  9: {
+    name: "Rocky Roll",
+    job: "Musician",
+    questions: [1,5,12,13,14],
+    sex: Sex.male
+  },
+  10: {
+    name: "Ling Tong",
+    job: "Restaurant Owner",
+    questions: [7,8,12,13,14],
+    sex: Sex.male
+  },
+  11: {
+    name: "Ivy Little",
+    job: "Landlady",
+    questions: [6,7,11,13,14],
+    sex: Sex.female
+  },
+  12: {
+    name: "Lucy Tumble",
+    job: "Secretary",
+    questions: [2,6,11,13,14],
+    sex: Sex.female
+  },
+  13: {
+    name: "Piper Perez",
+    job: "Latin Singer",
+    questions: [3,7,11,13,14],
+    sex: Sex.female
+  },
+  14: {
+    name: "Dina Racheti",
+    job: "Socialite",
+    question: [1,6,11,13,14],
+    sex: Sex.female
+  },
+  15: {
+    name: "Eileen Stellar",
+    job: "Actress",
+    questions: [4,7,11,13,14],
+    sex: Sex.female
+  },
+  16: {
+    name: "Joan Finefugle",
+    job: "Accountant",
+    questions: [6,8,10,13,14],
+    sex: Sex.female
+  },
+  17: {
+    name: "Rose Pettle",
+    job: "Gossip Columnist",
+    questions: [2,7,10,13,14],
+    sex: Sex.female
+  },
+  18: {
+    name: "Doris Dill",
+    job: "Newscaster",
+    questions: [1,8,10,13,14],
+    sex: Sex.female
+  },
+  19: {
+    name: "Candy Roll",
+    job: "Groupie",
+    questions: [3,8,10,13,14],
+    sex: Sex.female
+  },
+  20: {
+    name: "Sing Wong",
+    job: "Waitress",
+    questions: [4,8,10,13,14],
+    sex: Sex.female
+  }
+}
